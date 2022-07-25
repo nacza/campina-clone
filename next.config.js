670afcda8ee922a8a0a1e68f4bd14d1dac23a8a2
programmer <naca.anggitian22@gmail.com>
2022-07-25 23:28:21 +0700

    first push

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['icecreamstore.co.id', 'lh5.googleusercontent.com']
+  }
 }
 
 module.exports = nextConfig
