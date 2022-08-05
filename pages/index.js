3246526136527d900df6be9a7f7261861a9ef9b8
programmer <naca.anggitian22@gmail.com>
2022-08-06 01:23:21 +0700

    responsive navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
 import Image from "next/image";
-import MdcCartOutline from '@meronex/icons/mdc/MdcCartOutline';
+import {
+  IoSearchOutline,
+  IoCartOutline,
+  IoPersonCircleOutline,
+  IoMenuSharp,
+} from "react-icons/io5";
 
 export default function Home() {
+  const [focusSearch, setFocusSearch] = useState(false);
+
+  function onFocusSearch() {
+    setFocusSearch(!focusSearch);
+  }
+
+  function onBlurSearch() {
+    setFocusSearch(!focusSearch);
+  }
+
   return (
     <nav className="nav-header">
-      <ul className="nav-header__wrapper">
+      <ul className="nav-header__wrapper hidden md:flex">
         <li className="nav-header__item">HOME</li>
         <li className="nav-header__item">PRODUK</li>
         <li className="nav-header__item">PROMOSI</li>
@@ -13,36 +29,51 @@ export default function Home() {
       </ul>
 
       <div className="nav-header__logo">
-        <Image 
+        <Image
           className="nav-header__logo-inside"
           src="https://icecreamstore.co.id/assets/images/logo.png"
           width={100}
           height={74}
-          alt="logo" />
+          alt="logo"
+        />
       </div>
 
-      <div className="nav-header__search">
-        <input 
-          className="nav-header__search-input" 
-          placeholder="CARI PRODUK" 
-          type="text" 
-          name="search" />
+      <div className="nav-header__search w-full ml-3 sm:ml-0 md:w-auto">
+        <div className="mr-2 cursor-pointer p-2 hover:bg-gray-200 rounded duration-100 block md:hidden">
+          <IoMenuSharp size={25} />
+        </div>
+        <div
+          className={
+            focusSearch
+              ? "relative w-full flex justify-center items-center rounded-md pl-1 pr-5 border-2 duration-100 border-blue-300"
+              : "relative w-full flex justify-center items-center rounded-md pl-1 pr-5 border-2 duration-100"
+          }
+        >
+          <input
+            className="nav-header__search-input outline-none p-2"
+            placeholder="CARI PRODUK"
+            type="text"
+            name="search"
+            onFocus={onFocusSearch}
+            onBlur={onBlurSearch}
+          />
+          <div className="p-2 cursor-pointer bg-gray-200 absolute top-0 right-0 rounded-r">
+            <IoSearchOutline size={20} />
+          </div>
+        </div>
+        <div className="nav-header__search-cart-wrapper p-2 rounded mx-2 hover:bg-gray-200 duration-200">
+          <IoCartOutline className="nav-header__search-cart" size={25} />
+        </div>
 
-        <MdcCartOutline 
-          className="nav-header__search-cart"
-          size={25}/>
-
-        <p className="nav-header__search-cart-count">50</p>
-
-        <div className="nav-header__profile-wrapper">
-          <Image 
-          className="nav-header__profile-image"
-          src="https://lh5.googleusercontent.com/-u8Q4QSO84XQ/AAAAAAAAAAI/AAAAAAAAAAc/OOSySPGRvj8/photo.jpg"
-          width={100}
-          height={100}
-          alt="logo" />
+        <div className="nav-header__profile-wrapper flex rounded-full bg-gray-200 cursor-pointer duration-200 hidden sm:flex">
+          <div className="w-[40px] h-[40px]">
+            <img
+              className="rounded-full"
+              src="https://lh5.googleusercontent.com/-u8Q4QSO84XQ/AAAAAAAAAAI/AAAAAAAAAAc/OOSySPGRvj8/photo.jpg"
+            />
+          </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
