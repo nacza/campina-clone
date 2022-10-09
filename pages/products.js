b5f2e3aa66175d12d9441aa5309f155eba8c5272
natczh <naca.anggitian22@gmail.com>
2022-10-09 21:34:49 +0700

    add data to products

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,31 @@
+import CardProduct from "@/components/card-product";
 import Template from "@/components/template";
+import { getDataProducts } from "@/lib/data";
 
-function Products() {
+export async function getStaticProps() {
+  const { listProducts } = await getDataProducts();
+
+  return {
+    props: {
+      listProducts,
+    },
+  };
+}
+
+function Products({ listProducts }) {
   return (
     <Template>
-      <div>Products</div>
+      {/* Products */}
+      <div className="list-products grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 sm:gap-4">
+        {listProducts.map((item, i) => (
+          <CardProduct
+            key={item.id}
+            images={item.images[0].url}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </Template>
   );
 }
